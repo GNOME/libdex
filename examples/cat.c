57e7a13b8fbcc3e7b79cd3645567c783b0bf1c18
Christian Hergert <chergert@redhat.com>
2022-12-06 21:58:16 -0800

    examples: simplify cat example
    Make some shared code that we can port cat-aio to as well, and keep the
    example a bit shorter. I still want to experiment with a DexFiber
    eventually because we can make this code much easier to reason about,
    even if we reduce it to limited cases where m:n threading might be
    wanted.

--- examples/cat.c
+++ examples/cat.c
@@ -22,152 +22,128 @@
 #include <unistd.h>
 
 #include <gio/gio.h>
+#include <gio/gunixinputstream.h>
 #include <gio/gunixoutputstream.h>
 
-#include <libdex.h>
-
-#define BUFSIZE 4096*4
-
-static GFile         *file;
-static GInputStream  *input;
-static GOutputStream *output;
-static DexChannel    *channel;
-static GMainLoop     *main_loop;
-static gsize          size;
+#include "cat-util.h"
 
 static DexFuture *
-read_next (void)
+release_buffer (DexFuture *future,
+                gpointer user_data)
 {
-  g_assert (G_IS_INPUT_STREAM (input));
-
-  if (size == 0)
-    return dex_future_new_take_boxed (G_TYPE_BYTES, g_bytes_new_static ("", 0));
-
-  return dex_input_stream_read_bytes (input, MIN (BUFSIZE, size), 0);
-}
-
-static DexFuture *
-read_loop (DexFuture *future,
-           gpointer   user_data)
-{
-  DexFuture *bytes_future = dex_future_set_get_future_at (DEX_FUTURE_SET (future), 0);
-  const GValue *value = dex_future_get_value (bytes_future, NULL);
-
-  if (G_VALUE_HOLDS (value, G_TYPE_BYTES))
-    {
-      GBytes *bytes = g_value_get_boxed (value);
-
-      if (g_bytes_get_size (bytes) > 0)
-        {
-          size -= g_bytes_get_size (bytes);
-
-          return dex_future_all (read_next (),
-                                 dex_channel_send (channel, dex_ref (bytes_future)),
-                                 NULL);
-        }
-    }
-
-  dex_channel_close_send (channel);
-
+  Buffer *buffer = user_data;
+  cat_push_buffer (buffer->cat, buffer);
   return NULL;
 }
 
 static DexFuture *
-read_routine (gpointer user_data)
+cat_read_cb (DexFuture *future,
+             gpointer   user_data)
 {
-  return dex_future_then_loop (dex_future_all (read_next (), NULL),
-                               read_loop, NULL, NULL);
-}
+  DexFuture *read_future = dex_future_set_get_future_at (DEX_FUTURE_SET (future), 0);
+  const GValue *value = dex_future_get_value (read_future, NULL);
+  Cat *cat = user_data;
+  Buffer *buffer = g_steal_pointer (&cat->current);
 
-static DexFuture *
-write_loop (DexFuture *future,
-            gpointer   user_data)
-{
-  DexFuture *bytes_future = dex_future_set_get_future_at (DEX_FUTURE_SET (future), 0);
-  const GValue *value = dex_future_get_value (bytes_future, NULL);
-
-  if (value == NULL)
+  if (!(buffer->length = g_value_get_int64 (value)))
     return NULL;
 
-  return dex_future_all (dex_channel_receive (channel),
-                         dex_output_stream_write_bytes (output, g_value_get_boxed (value), 0),
+  cat->current = cat_pop_buffer (cat);
+
+  return dex_future_all (dex_input_stream_read (cat->input,
+                                                cat->current->data,
+                                                cat->current->capacity,
+                                                G_PRIORITY_DEFAULT),
+                         dex_channel_send (cat->channel,
+                                           dex_future_new_for_pointer (buffer)),
                          NULL);
 }
 
 static DexFuture *
-write_routine (gpointer user_data)
+cat_read (Cat *cat)
+{
+  cat->current = cat_pop_buffer (cat);
+
+  return dex_future_then_loop (dex_future_all (dex_input_stream_read (cat->input,
+                                                                      cat->current->data,
+                                                                      cat->current->capacity,
+                                                                      G_PRIORITY_DEFAULT),
+                                               NULL),
+                               cat_read_cb, cat, NULL);
+}
+
+static DexFuture *
+cat_read_routine (gpointer user_data)
+{
+  Cat *cat = user_data;
+  cat->input = G_INPUT_STREAM (g_unix_input_stream_new (cat->read_fd, FALSE));
+  return cat_read (cat);
+}
+
+static DexFuture *
+cat_write_cb (DexFuture *completed,
+              gpointer   user_data)
+{
+  Cat *cat = user_data;
+  DexFuture *buffer_future = dex_future_set_get_future_at (DEX_FUTURE_SET (completed), 0);
+  const GValue *value = dex_future_get_value (buffer_future, NULL);
+  Buffer *buffer;
+
+  if (value == NULL)
+    return NULL;
+
+  buffer = g_value_get_pointer (value);
+  return dex_future_all (dex_channel_receive (cat->channel),
+                         dex_future_finally (dex_output_stream_write (cat->output,
+                                                                      buffer->data,
+                                                                      buffer->length,
+                                                                      G_PRIORITY_DEFAULT),
+                                             release_buffer, buffer, NULL),
+                         NULL);
+}
+
+static DexFuture *
+cat_write (Cat *cat)
 {
   DexFuture *future;
 
-  future = dex_channel_receive (channel);
+  future = dex_channel_receive (cat->channel);
   future = dex_future_all (future, NULL);
-  future = dex_future_then_loop (future, write_loop, NULL, NULL);
+  future = dex_future_finally_loop (future, cat_write_cb, cat, NULL);
 
   return future;
 }
 
 static DexFuture *
-cat_finished (DexFuture *future,
-              gpointer   user_data)
+cat_write_routine (gpointer user_data)
 {
-  g_main_loop_quit (main_loop);
-  return NULL;
+  Cat *cat = user_data;
+  cat->output = G_OUTPUT_STREAM (g_unix_output_stream_new (cat->write_fd, FALSE));
+  return cat_write (cat);
 }
 
 int
 main (int   argc,
       char *argv[])
 {
-  const GValue *value;
-  DexFuture *future;
   GError *error = NULL;
-  GFileInfo *info = NULL;
+  Cat cat;
+  int ret;
 
   dex_init ();
 
-  if (argc != 2)
+  if (!cat_init (&cat, &argc, &argv, &error) ||
+      !cat_run (&cat,
+                dex_scheduler_spawn (NULL, cat_read_routine, &cat, NULL),
+                dex_scheduler_spawn (NULL, cat_write_routine, &cat, NULL),
+                &error))
     {
-      g_printerr ("usage: %s FILE\n", argv[0]);
-      return EXIT_FAILURE;
+      g_printerr ("cat: %s\n", error->message);
+      g_clear_error (&error);
+      ret = EXIT_FAILURE;
     }
 
-  main_loop = g_main_loop_new (NULL, FALSE);
-  file = g_file_new_for_commandline_arg (argv[1]);
+  cat_clear (&cat);
 
-  if (!(info = g_file_query_info (file, G_FILE_ATTRIBUTE_STANDARD_SIZE, 0, NULL, &error)))
-    {
-      g_printerr ("Failed to open %s: %s\n", argv[1], error->message);
-      return EXIT_FAILURE;
-    }
-
-  size = g_file_info_get_size (info);
-  output = g_unix_output_stream_new (STDOUT_FILENO, FALSE);
-  channel = dex_channel_new (4);
-
-  if (!(input = G_INPUT_STREAM (g_file_read (file, NULL, &error))))
-    {
-      g_printerr ("Failed to open %s: %s\n", argv[1], error->message);
-      return EXIT_FAILURE;
-    }
-
-  /* Spawn routines for read and write loops, communicate with channel */
-  future = dex_future_all (dex_scheduler_spawn (NULL, read_routine, NULL, NULL),
-                           dex_scheduler_spawn (NULL, write_routine, NULL, NULL),
-                           NULL);
-  future = dex_future_finally (future, cat_finished, NULL, NULL);
-
-  g_main_loop_run (main_loop);
-
-  if (!(value = dex_future_get_value (future, &error)))
-    {
-      g_printerr ("Failed to cat %s: %s\n", argv[1], error->message);
-      return EXIT_FAILURE;
-    }
-
-  dex_clear (&future);
-  g_clear_object (&file);
-  g_clear_object (&input);
-  g_clear_object (&output);
-
-  return EXIT_SUCCESS;
+  return ret;
 }
