3ab9af0a26b578d77bd305a7276b0cb6e415f28f
Christian Hergert <chergert@redhat.com>
2023-05-30 15:56:33 -0700

    gio: cleanup function alignments

--- src/dex-gio.h
+++ src/dex-gio.h
@@ -31,99 +31,99 @@ G_BEGIN_DECLS
 #define DEX_TYPE_INET_ADDRESS_LIST (dex_inet_address_list_get_type())
 
 DEX_AVAILABLE_IN_ALL
-GType      dex_file_info_list_get_type    (void) G_GNUC_CONST;
+GType      dex_file_info_list_get_type                 (void) G_GNUC_CONST;
 DEX_AVAILABLE_IN_ALL
-GType      dex_inet_address_list_get_type (void) G_GNUC_CONST;
+GType      dex_inet_address_list_get_type              (void) G_GNUC_CONST;
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_file_make_directory        (GFile                    *file,
-                                           int                       io_priority);
+DexFuture *dex_file_make_directory                     (GFile                    *file,
+                                                        int                       io_priority);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_file_copy                  (GFile                    *source,
-                                           GFile                    *destination,
-                                           GFileCopyFlags            flags,
-                                           int                       io_priority);
+DexFuture *dex_file_copy                               (GFile                    *source,
+                                                        GFile                    *destination,
+                                                        GFileCopyFlags            flags,
+                                                        int                       io_priority);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_file_read                  (GFile                    *file,
-                                           int                       io_priority);
+DexFuture *dex_file_read                               (GFile                    *file,
+                                                        int                       io_priority);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_file_load_contents_bytes   (GFile                    *file);
+DexFuture *dex_file_load_contents_bytes                (GFile                    *file);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_file_query_info            (GFile                    *file,
-                                           const char               *attributes,
-                                           GFileQueryInfoFlags       flags,
-                                           int                       io_priority);
+DexFuture *dex_file_query_info                         (GFile                    *file,
+                                                        const char               *attributes,
+                                                        GFileQueryInfoFlags       flags,
+                                                        int                       io_priority);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_file_replace               (GFile                    *file,
-                                           const char               *etag,
-                                           gboolean                  make_backup,
-                                           GFileCreateFlags          flags,
-                                           int                       io_priority);
+DexFuture *dex_file_replace                            (GFile                    *file,
+                                                        const char               *etag,
+                                                        gboolean                  make_backup,
+                                                        GFileCreateFlags          flags,
+                                                        int                       io_priority);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_file_enumerate_children    (GFile                    *file,
-                                           const char               *attributes,
-                                           GFileQueryInfoFlags       flags,
-                                           int                       io_priority);
+DexFuture *dex_file_enumerate_children                 (GFile                    *file,
+                                                        const char               *attributes,
+                                                        GFileQueryInfoFlags       flags,
+                                                        int                       io_priority);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_file_enumerator_next_files (GFileEnumerator          *file_enumerator,
-                                           int                       num_files,
-                                           int                       io_priority);
+DexFuture *dex_file_enumerator_next_files              (GFileEnumerator          *file_enumerator,
+                                                        int                       num_files,
+                                                        int                       io_priority);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_input_stream_close         (GInputStream             *self,
-                                           int                       io_priority);
+DexFuture *dex_input_stream_close                      (GInputStream             *self,
+                                                        int                       io_priority);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_input_stream_read          (GInputStream             *self,
-                                           gpointer                  buffer,
-                                           gsize                     count,
-                                           int                       io_priority);
+DexFuture *dex_input_stream_read                       (GInputStream             *self,
+                                                        gpointer                  buffer,
+                                                        gsize                     count,
+                                                        int                       io_priority);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_input_stream_read_bytes    (GInputStream             *self,
-                                           gsize                     count,
-                                           int                       io_priority);
+DexFuture *dex_input_stream_read_bytes                 (GInputStream             *self,
+                                                        gsize                     count,
+                                                        int                       io_priority);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_output_stream_close        (GOutputStream            *self,
-                                           int                       io_priority);
+DexFuture *dex_output_stream_close                     (GOutputStream            *self,
+                                                        int                       io_priority);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_output_stream_splice       (GOutputStream            *output,
-                                           GInputStream             *input,
-                                           GOutputStreamSpliceFlags  flags,
-                                           int                       io_priority);
+DexFuture *dex_output_stream_splice                    (GOutputStream            *output,
+                                                        GInputStream             *input,
+                                                        GOutputStreamSpliceFlags  flags,
+                                                        int                       io_priority);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_output_stream_write        (GOutputStream            *self,
-                                           gconstpointer             buffer,
-                                           gsize                     count,
-                                           int                       io_priority);
+DexFuture *dex_output_stream_write                     (GOutputStream            *self,
+                                                        gconstpointer             buffer,
+                                                        gsize                     count,
+                                                        int                       io_priority);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_output_stream_write_bytes  (GOutputStream            *self,
-                                           GBytes                   *bytes,
-                                           int                       io_priority);
+DexFuture *dex_output_stream_write_bytes               (GOutputStream            *self,
+                                                        GBytes                   *bytes,
+                                                        int                       io_priority);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_socket_listener_accept     (GSocketListener          *listener);
+DexFuture *dex_socket_listener_accept                  (GSocketListener          *listener);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_socket_client_connect      (GSocketClient            *socket_client,
-                                           GSocketConnectable       *socket_connectable);
+DexFuture *dex_socket_client_connect                   (GSocketClient            *socket_client,
+                                                        GSocketConnectable       *socket_connectable);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_io_stream_close            (GIOStream                *io_stream,
-                                           int                       io_priority);
+DexFuture *dex_io_stream_close                         (GIOStream                *io_stream,
+                                                        int                       io_priority);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_resolver_lookup_by_name    (GResolver                *resolver,
-                                           const char               *address);
+DexFuture *dex_resolver_lookup_by_name                 (GResolver                *resolver,
+                                                        const char               *address);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_bus_get (GBusType bus_type);
+DexFuture *dex_bus_get                                 (GBusType                  bus_type);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_dbus_connection_call (GDBusConnection    *connection,
-                                     const char         *bus_name,
-                                     const char         *object_path,
-                                     const char         *interface_name,
-                                     const char         *method_name,
-                                     GVariant           *parameters,
-                                     const GVariantType *reply_type,
-                                     GDBusCallFlags      flags,
-                                     int                 timeout_msec);
+DexFuture *dex_dbus_connection_call                    (GDBusConnection          *connection,
+                                                        const char               *bus_name,
+                                                        const char               *object_path,
+                                                        const char               *interface_name,
+                                                        const char               *method_name,
+                                                        GVariant                 *parameters,
+                                                        const GVariantType       *reply_type,
+                                                        GDBusCallFlags            flags,
+                                                        int                       timeout_msec);
 DEX_AVAILABLE_IN_ALL
-DexFuture *dex_dbus_connection_send_message_with_reply (GDBusConnection       *connection,
-                                                        GDBusMessage          *message,
-                                                        GDBusSendMessageFlags  flags,
-                                                        int                    timeout_msec,
-                                                        guint32               *out_serial);
+DexFuture *dex_dbus_connection_send_message_with_reply (GDBusConnection          *connection,
+                                                        GDBusMessage             *message,
+                                                        GDBusSendMessageFlags     flags,
+                                                        int                       timeout_msec,
+                                                        guint32                  *out_serial);
 
 G_END_DECLS
