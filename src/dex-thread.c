b9d5af7a6f5f4601c50149ffae3a625fa1f445b3
Christian Hergert <chergert@redhat.com>
2025-06-26 11:42:41 -0700

    thread: fix leak of future in dex_thread_wait_for()
    We take ownership of @future, so make sure we release the waiter (when
    using that) or the future when short-circuiting.

--- src/dex-thread.c
+++ src/dex-thread.c
@@ -187,19 +187,25 @@ gboolean
 dex_thread_wait_for (DexFuture  *future,
                      GError    **error)
 {
-  DexWaiter *waiter;
   gboolean ret;
 
   g_return_val_if_fail (DEX_IS_FUTURE (future), FALSE);
 
   /* Short-circuit when @future is already completed */
   if (!dex_future_is_pending (future))
-    return !!dex_future_get_value (future, error);
+    {
+      ret = !!dex_future_get_value (future, error);
+      dex_unref (future);
+    }
+  else
+    {
+      DexWaiter *waiter;
 
-  waiter = dex_waiter_new (future);
-  dex_waiter_wait (waiter);
-  ret = !!dex_future_get_value (DEX_FUTURE (waiter), error);
-  dex_unref (waiter);
+      waiter = dex_waiter_new (future);
+      dex_waiter_wait (waiter);
+      ret = !!dex_future_get_value (DEX_FUTURE (waiter), error);
+      dex_unref (waiter);
+    }
 
   return ret;
 }
