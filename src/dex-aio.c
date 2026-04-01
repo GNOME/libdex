GNOME/libdex

commit 165e777a8a0b021b4691c9973a66b362a5d8cac4

aio: improve dex aio function docs

src/dex-aio.c
@@ -41,10 +41,15 @@ dex_aio_context_current (void)
 
 /**
  * dex_aio_read:
- * @buffer: (array length=count) (element-type guint8) (out caller-allocates)
- * @count: (in)
+ * @aio_context: (nullable):
+ * @buffer: (array length=count) (element-type guint8) (out caller-allocates):
+ * @count: the number of bytes to read
+ * @offset: the positioned offset within @fd to read from
  *
  * An asynchronous `pread()` wrapper.
+ *
+ * Generally you want to provide `NULL` for the @aio_context as that
+ * will get the default aio context for your scheduler.
  *
  * Returns: (transfer full): a future that will resolve when the
  *   read completes or rejects with error.
@@ -65,9 +70,15 @@ dex_aio_read (DexAioContext *aio_context,
 
 /**
  * dex_aio_write:
- * @buffer: (array length=count) (element-type guint8)
+ * @aio_context: (nullable):
+ * @buffer: (array length=count) (element-type guint8):
+ * @count: the number of bytes to write from @buffer
+ * @offset: the positioned offset within @fd to write at
  *
  * An asynchronous `pwrite()` wrapper.
+ *
+ * Generally you want to provide `NULL` for the @aio_context as that
+ * will get the default aio context for your scheduler.
  *
  * Returns: (transfer full): a future that will resolve when the
  *   write completes or rejects with error.

commit 0d1e1052d01763b5608fb9a20940499eb31d45b9

Fix some annotation regressions
These were added in commit d7ded55395057de0837020c0e9a091866d3de8c1, but
regressed with 165e777a8a0b021b4691c9973a66b362a5d8cac4,
7ccc4ef641003c89fcdf268bf6355a7c108ee190 and
b3787e1379183000c2fce223a7412df01c0c618e.

See also https://gitlab.gnome.org/GNOME/gobject-introspection/-/issues/546

Signed-off-by: Sergey Bugaev <bugaevc@gmail.com>

src/dex-aio.c
@@ -43,7 +43,7 @@ dex_aio_context_current (void)
  * dex_aio_read:
  * @aio_context: (nullable):
  * @buffer: (array length=count) (element-type guint8) (out caller-allocates):
- * @count: the number of bytes to read
+ * @count: (in): the number of bytes to read
  * @offset: the positioned offset within @fd to read from
  *
  * An asynchronous `pread()` wrapper.

src/dex-gio.c
@@ -472,7 +472,7 @@ dex_input_stream_read_cb (GObject      *object,
  * dex_input_stream_read:
  * @self: a [class@Gio.InputStream]
  * @buffer: (array length=count) (element-type guint8) (out caller-allocates)
- * @count: size in bytes to read from the stream into the @buffer
+ * @count: (in): size in bytes to read from the stream into the @buffer
  * @io_priority: the [IO priority][iface@Gio.AsyncResult#io-priority] of the
  *   request
  *